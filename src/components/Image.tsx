a767f831bf76abc6a7e260da709d083f6b3f50aa
Felipe <riopipe8@gmail.com>
2023-06-14 23:59:56 -0400

    styling layout

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,7 +3,8 @@ import "./Image.css"
 function Image() {
   return (
     <div className="img-container">
-			<img src="/src/assets/img/illustration-sign-up-mobile.svg" alt="image" />
+			<img className="img-mobile" src="/src/assets/img/illustration-sign-up-mobile.svg" alt="image" />
+			<img className="img-desktop" src="/src/assets/img/illustration-sign-up-desktop.svg" alt="image" />
 		</div>
   )
 }
