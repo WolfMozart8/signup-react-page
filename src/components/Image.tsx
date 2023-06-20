156e329869df246abd864572b9dae3dce6da6502
Felipe <riopipe8@gmail.com>
2023-06-20 17:30:12 -0400

    images fixed

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,8 +3,8 @@ import "./Image.css"
 function Image() {
   return (
     <div className="img-container">
-			<img className="img-mobile" src="/img/illustration-sign-up-mobile.svg" alt="image" />
-			<img className="img-desktop" src="/img/illustration-sign-up-desktop.svg" alt="image" />
+			<img className="img-mobile" src="../img/illustration-sign-up-mobile.svg" alt="image" />
+			<img className="img-desktop" src="../img/illustration-sign-up-desktop.svg" alt="image" />
 		</div>
   )
 }
