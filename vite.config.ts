926e13a2bdfcbfee3ad57f663ff6d14d04b46722
Felipe <riopipe8@gmail.com>
2023-06-20 18:36:09 -0400

    fixed images path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/signup-react-page/",
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+
   plugins: [react()],
 })
